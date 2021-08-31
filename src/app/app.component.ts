c808a391564fe9cc42ab445e71fc8d99d264cd4c
Arul dass <aruldass@nainatechnologies.com>
2021-08-31 14:52:29 +0530

    base code added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { ApplicationRef, Component, OnInit } from '@angular/core';
+
+import { SwUpdate, SwPush } from '@angular/service-worker';
+import { ToastrService } from 'ngx-toastr';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'PWA-Project';
+  private publicKey = "BOGJ3y09Gb2jQcvqHCsuj04AWombIIBiHceWkWMz_5n1kUPp0wmhwYkY7qdpSy3rpgUue8-wsOwzlY-msrXm8ME"
+  constructor(private update: SwUpdate,
+    private appRef: ApplicationRef,
+    private toastr: ToastrService,
+    private swPush: SwPush) { }
+
+  ngOnInit() {
+
+    // this.showSuccess();
+
+    this.pushSubscription();
+
+    this.swPush.messages.subscribe((message: any) => {
+      let msg = message.notification.data;
+      console.log('message------->>', msg);
+      this.toastr.success(msg.title, msg.url);
+    }
+    );
+
+    this.swPush.notificationClicks.subscribe(({ action, notification }) => {
+      window.open(notification.data.url);
+    });
+
+  }
+
+  // showSuccess() {
+  //   this.toastr.success('Hello world!', 'Toastr fun!');
+  // }
+
+  pushSubscription() {
+    if (!this.swPush.isEnabled) {
+      console.log('Notification is not enabled');
+      return;
+    }
+
+    this.swPush
+      .requestSubscription({
+        serverPublicKey: this.publicKey,
+      })
+      .then((sub) => {
+        // Make a post call to serve
+        console.log(JSON.stringify(sub));
+      })
+      .catch((err) => console.log(err));
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +15,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot(), // ToastrModule added
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      registrationStrategy: 'registerImmediately'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
